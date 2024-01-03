f2e7b5c1035f59f72295836558d6f5f78db5de31
Michal Korman <michal.korman@icloud.com>
2024-01-03 23:42:30 +0100

    lint

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -7,7 +7,7 @@ const Error = () => {
     <div className="flex flex-col w-screen justify-center items-center gap-6 text-white">
       <MdError size={48} />
       <span className="text-3xl">Oops</span>
-      <span className="text-xl">{'Something went wrong, please try again later'}</span>
+      <span className="text-xl">Something went wrong, please try again later</span>
     </div>
   );
 };
